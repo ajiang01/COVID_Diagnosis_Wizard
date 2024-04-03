4268a29c349258aaa4237e98b977a203abb30465
Kevin Chan <kevin@kevins-air.lan>
2024-04-03 14:03:08 -0400

    Added random_file.py

---
+++ random_file.py
@@ -0,0 +1,9 @@
+class Person:
+    """ A person in the social network
+    
+        Attributes:
+        name(str): the person's name
+        connections (set of Person): other people in the social network who
+        knows this person
+    """
+    
