48e98e5168acbcc95f92511a3d13396aa124536e
Allen Jiang <ajiang01@terpmail.umd.edu>
2024-04-10 18:46:50 -0400

    edited survey iterations from 1 to 3

--- covid_diagnosis_wizard.py
+++ covid_diagnosis_wizard.py
@@ -1,6 +1,6 @@
 def survey():
     with open("survey_responses.txt", "w", encoding = "utf-8") as file:
-        for responses in range (1):
+        for responses in range (3):
             name = input("Enter your name: ")
             file.write(f"{name}\n")
             
