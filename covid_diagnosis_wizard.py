8eadc32d801f189bc18ba295e773bbb9480a1183
Allen Jiang <ajiang01@terpmail.umd.edu>
2024-04-10 18:45:06 -0400

    created project file and survey

---
+++ covid_diagnosis_wizard.py
@@ -0,0 +1,27 @@
+def survey():
+    with open("survey_responses.txt", "w", encoding = "utf-8") as file:
+        for responses in range (1):
+            name = input("Enter your name: ")
+            file.write(f"{name}\n")
+            
+            dob = input("Enter your date of birth. Use mm/dd/yy format: ")
+            file.write(f"{dob}\n")
+            
+            cough = input("(Yes/No) Exhibiting persistent coughing: ")
+            file.write(f"{cough}\n")
+            
+            fever = input("(Yes/No) Exhibiting fever: ")
+            file.write(f"{fever}\n")
+            
+            nausea = input("(Yes/No) Exhibiting nausea: ")
+            file.write(f"{nausea}\n")
+            
+            sore_throat = input("(Yes/No) Exhibiting sore throat: ")
+            file.write(f"{sore_throat}\n")
+            
+survey()
+            
+            
+            
+            
+            
