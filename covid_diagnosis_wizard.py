3b0818c0c241cf8c085a0c350a508c0d04985688
Kevin Chan <kevin@kevins-air.lan>
2024-04-11 12:42:36 -0400

    added get_responses function

--- covid_diagnosis_wizard.py
+++ covid_diagnosis_wizard.py
@@ -1,3 +1,22 @@
+def get_response(symptoms_file, user_symptoms):
+    with open(symptoms_file, 'r') as file:
+        symptoms_dict = {}
+        for line in file:
+            symptom, response = line.strip().split(':')
+            symptoms_dict[symptom.strip()] = response.strip()
+
+    yes_count = sum(1 for symptom in user_symptoms if symptom.lower() == 'yes')
+
+    if yes_count >= 2:
+        response = "You seem to likely have COVID-19."
+    elif yes_count == 1:
+        response = "There's a possibility you might have COVID-19. Take \
+            precautions and cconsider getting tested ASAP!!!"
+    else:
+        response = "You most likely don't have COVID-19."
+
+    print(f"Based on your responses, {response}")
+
 def survey():
     with open("survey_responses.txt", "w", encoding = "utf-8") as file:
         for responses in range (3):
